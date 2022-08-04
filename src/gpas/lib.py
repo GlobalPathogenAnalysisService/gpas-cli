067a57ec24a8398777bc88c2e9a9fdb4fd81e36e
Bede Constantinides <1007616+bede@users.noreply.github.com>
2022-08-04 16:13:42 +0100

    Finish implementing 7209aec452c5758d81ac7b049a72dba73224d178

--- src/gpas/lib.py
+++ src/gpas/lib.py
@@ -376,12 +376,12 @@ class Sample:
     def _get_convert_bam_cmd(self, paired=False) -> str:
         prefix = Path(self.working_dir) / Path(self.sample_name)
         if not self.paired:
-            cmd = f'{self.samtools_path} fastq -0 "{prefix.with_suffix(".fastq.gz")}" "{self.bam}"'
+            cmd = f'"{self.samtools_path}" fastq -0 "{prefix.with_suffix(".fastq.gz")}" "{self.bam}"'
             self.fastq = self.working_dir / Path(self.sample_name + ".fastq.gz")
         else:
             cmd = (
-                f'{self.samtools_path} sort -n "{self.bam}" |'
-                f" {self.samtools_path} fastq -N"
+                f'"{self.samtools_path}" sort -n "{self.bam}" |'
+                f' "{self.samtools_path}" fastq -N'
                 f' -1 "{prefix.parent / (prefix.name + "_1.fastq.gz")}"'
                 f' -2 "{prefix.parent / (prefix.name + "_2.fastq.gz")}"'
             )
